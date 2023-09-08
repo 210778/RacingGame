6ec35e4d782a89cb0942852accbf7641df156806
龍輝 大和田 <210778@jc-21.jp>
2023-09-08 16:36:11 +0900

    画像表示しなくては

--- ImagePrinter.cpp
+++ ImagePrinter.cpp
@@ -1,1 +1,33 @@
 #include "ImagePrinter.h"
+
+//コンストラクタ
+ImagePrinter::ImagePrinter(GameObject* parent)
+    :GameObject(parent, "ImagePrinter")
+
+{
+}
+
+//デストラクタ
+ImagePrinter::~ImagePrinter()
+{
+}
+
+//初期化
+void ImagePrinter::Initialize()
+{
+}
+
+//更新
+void ImagePrinter::Update()
+{
+}
+
+//描画
+void ImagePrinter::Draw()
+{
+}
+
+//開放
+void ImagePrinter::Release()
+{
+}
--- ImagePrinter.h
+++ ImagePrinter.h
@@ -1,5 +1,55 @@
 #pragma once
-class ImagePrinter
-{
-};
+#include <unordered_map>
+#include "Engine/GameObject.h"
 
+//◆◆◆を管理するクラス
+class ImagePrinter : public GameObject
+{
+    struct ImageData
+    {
+        int imageHandle_;
+        Transform imageTransform_;
+        int imageAlpha_;  //0 ~ 255
+
+        Transform changeTransform_;
+        int changeAlpha_;
+
+        ImageData()
+        {
+            imageHandle_ = -1;
+            imageAlpha_ = 255;
+            changeAlpha_ = 0;
+        }
+        ImageData(int handle, Transform transform, Transform cTransform, int alpha = 255, int cAlpha = 0)
+        {
+            imageHandle_ = handle;
+            imageTransform_ = transform;
+            imageAlpha_ = alpha;
+            changeTransform_ = cTransform;
+            changeAlpha_ = cAlpha;
+        }
+    }imageData_;
+
+    //画像
+    std::unordered_map<std::string, ImageData> imageMap_;
+
+
+public:
+    //コンストラクタ
+    ImagePrinter(GameObject* parent);
+
+    //デストラクタ
+    ~ImagePrinter();
+
+    //初期化
+    void Initialize() override;
+
+    //更新
+    void Update() override;
+
+    //描画
+    void Draw() override;
+
+    //開放
+    void Release() override;
+};
